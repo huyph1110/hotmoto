7452e1176b356ec321ff655dad34784c0929acd3
Huy <huyph.mobile@gmail.com>
2018-03-25 08:52:08 +0700

    no message

---
+++ hotmoto/hotmoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hotmoto
+//
+//  Created by Huy on 3/25/18.
+//  Copyright © 2018 Huy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
